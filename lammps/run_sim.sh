439176c7e44dfe5d7a037ed915d65904db754e41
Ubuntu <ubuntu@ip-172-31-37-119.eu-central-1.compute.internal>
2025-07-01 00:05:48 +0000

    1

--- lammps/run_sim.sh
+++ lammps/run_sim.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# filepath: run_lammps.sh
-
 # Check if temperature argument is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <temperature>"
@@ -11,14 +9,5 @@ fi
 # Get temperature from command line argument
 TEMP=$1
 
-# Validate that temperature is a number
-if ! [[ "$TEMP" =~ ^[0-9]+$ ]]; then
-    echo "Error: Temperature must be a positive integer"
-    exit 1
-fi
-
-# Clone repository, navigate to directory, substitute temperature, and run LAMMPS
-# git clone https://github.com/CamberCloud-Inc/prod_apps.git && \
-# cd prod_apps/lammps/ && \
 sed -i "s/\$(temperature)/$TEMP/g" unbreakable.lmp && \
 lmp -in unbreakable.lmp
