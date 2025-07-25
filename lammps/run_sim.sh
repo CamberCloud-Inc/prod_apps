45611fb76e478d6dd1765453a351a6d02d07d1dd
AWS ParallelCluster user <ec2-user@login.hpc.dev.camber.cloud>
2025-07-25 20:06:15 +0000

    CNT lammps & nexflow apps content improving iteration

--- lammps/run_sim.sh
+++ lammps/run_sim.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-# Check if temperature argument is provided
+# Check if breakVelocity argument is provided
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <temperature>"
-    echo "Example: $0 100"
+    echo "Usage: $0 <breakVelocity>"
+    echo "Example: $0 0.0005"
     exit 1
 fi
 
-# Get temperature from command line argument
-TEMP=$1
+# Get breakVelocity from command line argument
+BREAK_VELOCITY=$1
 
-sed -i "s/\$(temperature)/$TEMP/g" unbreakable.lmp && \
+sed -i "s/\${breakVelocity}/$BREAK_VELOCITY/g" unbreakable.lmp && \
 lmp -in unbreakable.lmp
+
+
+
+    // {
+    //   "type": "Select",
+    //   "label": "Breakup Velocity",
+    //   "name": "breakVelocity",
+    //   "description": "Sets the velocity of the front and back faces of the nanotube, aiming to tear it apart",
+    //   "defaultValue": "None",
+    //   "hidden": false,
+    //   "required": true,
+    //   "disabled": false,
+    //   "options": [
+    //     {
+    //       "label": "None",
+    //       "value": "0"
+    //     },
+    //     {
+    //       "label": "Weak",
+    //       "value": "0.0005"
+    //     },
+    //     {
+    //       "label": "Strong",
+    //       "value": "0.001"
+    //     }
+    //   ]
+    // }
